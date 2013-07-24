e5789087fd256eadbd9bc341f2254fbd5f7da7cd
db <dbdbdbdblive@gmail.com>
2013-07-24 00:34:52 +0000

    Read index.html from fs and send its contents as the response.

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fn = "index.html"
+  var fb = fs.readFileSync(fn);
+
+  response.send(fb.toString('utf8'));
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
